5ad07b2905b9c39d0e8ab5935063bbb7aa31c744
Raymond-P <raymondperez158@gmail.com>
2018-02-18 13:49:28 -0500

    added solution to input age

--- Python_200_Problems/basic/input_age.py
+++ Python_200_Problems/basic/input_age.py
@@ -2,4 +2,20 @@
 # python best courses https://courses.tanpham.org/
 # ---------------------------------------------------------------
 # Create a program that asks the user to enter their name and their age.
-# Print out a message addressed to them that tells them the year that they will turn 100 years old.
+# Print out a message addressed to them that tells them the year that they
+# will turn 100 years old.
+from datetime import date
+
+name = input("hello user. What is your name? ")
+print(f"Nice to meet you {name}!\n")
+age = None
+while not isinstance(age, int):
+    try:
+        age = int(input(f"say, How old are you {name}?"))
+    except ValueError:
+        print(f"{name} please answer with an integer number.")
+cur_year = date.today().year
+century_anum = cur_year + 100 - age
+if(input(f"Did your brithday already passed {name}? [y/n] ") != "y"):
+    century_anum -= 1
+print(f"\n{name}, you will become 100 years old in the year {century_anum}")
