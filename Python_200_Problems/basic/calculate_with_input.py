63f2849f933845bd68a06b4844cf78be588ad8b6
Raymond-P <raymondperez158@gmail.com>
2018-02-18 13:49:50 -0500

    added solution to calculate with input

--- Python_200_Problems/basic/calculate_with_input.py
+++ Python_200_Problems/basic/calculate_with_input.py
@@ -1,6 +1,18 @@
 # ---------------------------------------------------------------
 # python best courses https://courses.tanpham.org/
 # ---------------------------------------------------------------
-# Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn. Go to the editor
+# Write a Python program that accepts an integer (n) and computes the value of
+# n+nn+nnn. Go to the editor
 # Sample value of n is 5
 # Expected Result : 615
+
+
+def calc_input():
+    usr_in = input()
+    # try:
+    n = int(usr_in)
+    return n + (n * n) + (n * n * n)  # n + (n**2) + (n**3)
+
+
+if __name__ == '__main__':
+    print(calc_input())
